d7780c9f185c8f53bf71efbe86f707a8f2cbb454
200100306 <200100306@esg.ipsantarem.pt>
2024-11-12 14:46:21 +0000

    add

--- RentLuxCar/RentLuxCar/Website_TI/obj/Release/net8.0/Website_TI.AssemblyInfo.cs
+++ RentLuxCar/RentLuxCar/Website_TI/obj/Release/net8.0/Website_TI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Website_TI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d18ecb71b0a37dbb071cafb82c25e151686b0875")]
 [assembly: System.Reflection.AssemblyProductAttribute("Website_TI")]
